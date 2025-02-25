58627f0d935e051087335124e3860e71fe0191ac
HoMeR-c95 <talelzamouri19@gmail.com>
2025-02-25 18:49:33 +0100

    done

--- mobile/backend/src/index.js
+++ mobile/backend/src/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 // const userRoutes = require("./routes/userRoutes");
-
+const productRoutes = require("./routes/productRoutes");
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/products", productRoutes);
 // Routes
 // app.use("/api/users", userRoutes);
 
---
+++ mobile/frontend/types/Profile.ts
@@ -0,0 +1,18 @@
+import { User } from './User';
+
+export interface Profile {
+    id: number;
+    userId: number;
+    user: User;
+    firstName: string;
+    lastName: string;
+    bio?: string;
+    imageId?: number;
+    gender?: 'MALE' | 'FEMALE';
+    review?: string;
+    isAnonymous?: boolean;
+    isBanned?: boolean;
+    isVerified?: boolean;
+    isOnline?: boolean;
+    preferredCategories?: string;
+}
