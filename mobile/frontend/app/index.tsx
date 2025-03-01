d23487c5fd286d5f0689f47c24cd6b3835c2a795
BochraBenRomdhane <bochrabenromdhane2003@gmail.com>
2025-03-01 22:30:12 +0100

    10:35pm

--- mobile/frontend/app/index.tsx
+++ mobile/frontend/app/index.tsx
@@ -1,22 +1,20 @@
-import HomeScreen from '../screens/HomeScreen';
 import { Grid } from '../constants/Grid';
 import { Colors } from '../constants/Colors';
 import { StyleSheet } from 'react-native';
-import { Posts } from '../components/posts';
 import RootLayout from './_layout';
 import Login from "./auth/login"
 // Example usage in styles
-// const styles = StyleSheet.create({
-//   container: {
-//     padding: Grid.padding.horizontal,
-//     backgroundColor: Colors.light.background,
-//   },
-//   card: {
-//     borderRadius: Grid.card.borderRadius,
-//     padding: Grid.card.padding,
-//     marginBottom: Grid.spacing.md,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    padding: Grid.padding.horizontal,
+    backgroundColor: Colors.light.background,
+  },
+  card: {
+    borderRadius: Grid.card.borderRadius,
+    padding: Grid.card.padding,
+    marginBottom: Grid.spacing.md,
+  },
+});
 export default function Index() {
   return <Login />;
 }
