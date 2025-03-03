91188bd8c07941426f5414b5004f8eb40bdce678
HoMeR-c95 <talelzamouri19@gmail.com>
2025-03-03 14:02:21 +0100

    done

--- mobile/frontend/navigation/types.ts
+++
@@ -1,6 +0,0 @@
-export type RootStackParamList = {
-    'profile/edit': undefined; // Route for editing profile
-    'Profile': undefined; 
-    'profile/change': undefined; // Route for profile page
-    // ... other routes
-}; 
