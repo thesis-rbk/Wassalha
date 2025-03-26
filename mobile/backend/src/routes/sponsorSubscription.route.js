fd60cb9237d48675519059afe33d158e20d635e3
Firas Jebri <fiiraas.jeebrii@gmail.com>
2025-03-26 22:47:27 +0100

    seeds refactored

--- mobile/backend/src/routes/sponsorSubscription.route.js
+++ mobile/backend/src/routes/sponsorSubscription.route.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fetch = require("../controllers/fetch")
-const sponsor = require("../controllers/SponsorSubscription.js")
-const { authenticateUser } = require("../middleware/middleware")
-const midd = require("../middleware/middlewareSponsor")
-router.get("/besttarveler", fetch.All)
-router.get("/filter", fetch.searchRequestsTraveler)
-router.post("/createSponsor", midd.uerSPONSOR, sponsor.createSponsorShip)
-router.post("/createSub", sponsor.createSubscription)
-router.get("/allreview/:id", sponsor.rewiewSubscription)
-router.post("/createReview", sponsor.reviewCreate)
-router.get("/allSub", sponsor.allSub)
-router.get("/search", sponsor.search)
-router.get("/allCat", sponsor.getAllCategories)
-router.get("/allNot/:id", sponsor.getAllNotificationById)
-router.post("/payment", authenticateUser, sponsor.paymentSponsor)
-router.get("/checkSponsor", midd.uerSPONSOR, sponsor.checkSponsor)
-router.get("/one/:id", sponsor.findOneSponsor)
-router.get("/allRev/:id", sponsor.sponsorShipReview)
-router.post("/payment_Konnect", sponsor.initiatePayment)
-router.post("/createOrder", authenticateUser, sponsor.createOrderSponsor)
-router.get("/requests", authenticateUser, sponsor.getAllRequestsSponsor)
+const fetch = require("../controllers/fetch");
+const sponsor = require("../controllers/SponsorSubscription.js");
+const { authenticateUser } = require("../middleware/middleware");
+const midd = require("../middleware/middlewareSponsor");
+router.get("/besttarveler", fetch.All);
+router.get("/filter", fetch.searchRequestsTraveler);
+router.post("/createSponsor", midd.uerSPONSOR, sponsor.createSponsorShip);
+router.post("/createSub", sponsor.createSubscription);
+router.get("/allreview/:id", sponsor.rewiewSubscription);
+router.post("/createReview", sponsor.reviewCreate);
+router.get("/allSub", sponsor.allSub);
+router.get("/search", sponsor.search);
+router.get("/allCat", sponsor.getAllCategories);
+router.get("/allNot/:id", sponsor.getAllNotificationById);
+router.post("/payment", authenticateUser, sponsor.paymentSponsor);
+router.get("/checkSponsor", midd.uerSPONSOR, sponsor.checkSponsor);
+router.get("/one/:id", sponsor.findOneSponsor);
+router.get("/allRev/:id", sponsor.sponsorShipReview);
+router.post("/payment_Konnect", sponsor.initiatePayment);
+router.post("/createOrder", authenticateUser, sponsor.createOrderSponsor);
+router.get("/requestsSponsor", authenticateUser, sponsor.getAllRequestsSponsor);
 module.exports = router;
