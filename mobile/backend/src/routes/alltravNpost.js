68a32eaf85a1259c3e44a8ec9652adf01438b328
fahmi-najjari <fahminajjari@outlook.com>
2025-04-16 22:23:05 +0100

    deleted not needed files in the backend

--- mobile/backend/src/routes/alltravNpost.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const allpos = require("../controllers/alltravNpost");
-router.get("/", allpos.allorders);
-
-module.exports = router;
